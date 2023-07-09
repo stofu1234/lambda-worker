560eba967d3ffd67450cfe2d3cb9e367705fed1c
Naomasa Takasaki <yamaboku@gmail.com>
2023-07-09 23:24:23 +0900

    変数名変更

--- LambdaWorker.js
+++ LambdaWorker.js
@@ -20,11 +20,11 @@ export default class LambdaWorker {
     // WebWorker初期化
     const workerBlob = new Blob([workerScript], { type: 'text/plain' });
     const workerUrl = URL.createObjectURL(workerBlob);
-    this.worker = new Worker(workerUrl);
+    this.webWorker = new Worker(workerUrl);
     
     // 処理結果、受信イベント
     const parent = this;
-    this.worker.addEventListener('message', function(e) {
+    this.webWorker.addEventListener('message', function(e) {
       parent.returnPromise = new Promise((resolve, reject) => {
         if(e.data instanceof Error) {
           reject(e.data);
@@ -36,7 +36,7 @@ export default class LambdaWorker {
   }
   
   postMessage = (...postData) => {
-    this.worker.postMessage(postData);
+    this.webWorker.postMessage(postData);
   }
 }
 
